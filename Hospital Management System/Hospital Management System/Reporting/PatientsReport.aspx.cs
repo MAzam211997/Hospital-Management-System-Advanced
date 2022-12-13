6861f5c29c286161d5abf1c80859dc43b2262454
Muhammad Azam <mazam19971@gmail.com>
2022-12-14 00:56:54 +0500

    Added Patient Report

---
+++ Hospital Management System/Hospital Management System/Reporting/PatientsReport.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Hospital_Management_System.Models;
+using Microsoft.Reporting.WebForms;
+
+namespace Hospital_Management_System.Reporting
+{
+    public partial class PatientsReport : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ReportViewer1.ProcessingMode = ProcessingMode.Local;
+                ReportViewer1.InternalBorderStyle = BorderStyle.Solid;
+                ReportViewer1.InternalBorderStyle = BorderStyle.Solid;
+                ReportViewer1.ToolBarItemBorderStyle = BorderStyle.Solid;
+                ReportViewer1.LocalReport.ReportPath = Server.MapPath("DataSets/PatientsReport.rdlc");
+                var entities = new ApplicationDbContext();
+                ReportDataSource datasource = new ReportDataSource("PatientsDataSet", (from patients in entities.Patients
+                                                                               select patients));
+                ReportViewer1.LocalReport.DataSources.Clear();
+                ReportViewer1.LocalReport.DataSources.Add(datasource);
+
+            }
+        }
+    }
+}
